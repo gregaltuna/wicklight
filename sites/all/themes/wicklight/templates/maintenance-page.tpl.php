b17cacc127ad3c4b4751b892ebbea352b5dd5116
Greg Altuna <gregory.altuna@unt.edu>
2018-10-08 10:28:57 -0500

    initial upload from existing site

---
+++ sites/all/themes/wicklight/templates/maintenance-page.tpl.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * @file
+ * Alpha's theme implementation to display a single Drupal page.
+ */
+?>
+<table width="100%" height="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td style="text-align: center;"><img src="sites/all/themes/wicklight/images/wicklight-coming-soon.jpg" width="400" height="496" style="margin: 0 auto;" /></td>
+  </tr>
+</table>
